ecca2a8dc3961e53bc725148178dfc2bda157fc9
yashipant <123501654+yashi-star@users.noreply.github.com>
2024-08-25 13:58:43 +0530

    Add files via upload

---
+++ moviesite/finalmoviesite/index.js
@@ -0,0 +1,18 @@
+//slider
+
+let cardContainers = [...document.querySelectorAll(".card-container")];
+let preBtns = [...document.querySelectorAll(".pre-btn")];
+let nxtBtns = [...document.querySelectorAll(".nxt-btn")];
+
+cardContainers.forEach((item, i) => {
+  let containerDimensions = item.getBoundingClientRect();
+  let containerWidth = containerDimensions.width;
+
+  nxtBtns[i].addEventListener("click", () => {
+    item.scrollLeft += containerWidth;
+  });
+
+  preBtns[i].addEventListener("click", () => {
+    item.scrollLeft -= containerWidth;
+  });
+});
