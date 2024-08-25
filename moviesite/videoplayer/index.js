bfaa96085d83736380654ab8451449466dc3c8dc
yashipant <123501654+yashi-star@users.noreply.github.com>
2024-08-25 14:01:36 +0530

    Add files via upload

---
+++ moviesite/videoplayer/index.js
@@ -0,0 +1,27 @@
+/*let playBtns = document.querySelector(".play-btn-container ");
+let play = document.querySelector(".clip");
+let closeBtns = document.querySelectorAll(".close-movie ");
+
+playBtns.onclick = function () {
+  playBtns.classList.add("active");
+  play.classList.add("active");
+};
+closeBtns.onclick = function () {
+  play.classList.remove("active");
+  closeBtns.classList.remove("active");
+};
+
+/const video = document.getElementById("m-video");
+document.getElementById("play-btn").addEventListener("click", function () {
+  video.play();
+});
+document
+  .getElementById("play-btn:hover")
+  .addEventListener("click", function () {
+    video.play();
+  });
+
+document.getElementById("close-movie").addEventListener("click", function () {
+  video.pause();
+});
+*/
